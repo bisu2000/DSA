185b90b8f736a8d8e35a723866ef269bb6eff64c
bisu2000 <162682435+bisu2000@users.noreply.github.com>
2026-06-01 17:24:25 +0530

    Time: 26 ms (81.65%) | Memory: 107.5 MB (47.06%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+
+        for(int asteroid : asteroids) {
+            if(mass < asteroid)
+                return false;
+            else if(mass >= 1e5)
+                return true;
+
+            mass += asteroid;
+        }
+
+        return true;
+    }
+}
