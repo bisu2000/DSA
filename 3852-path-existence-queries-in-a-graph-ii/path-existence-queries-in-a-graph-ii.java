a1f641f40a9b713efbe2785a7010cd8e969a1bc4
Biswajit Manna <162682435+bisu2000@users.noreply.github.com>
2026-07-10 20:57:54 +0530

    Time: 94 ms (87.50%) | Memory: 269.3 MB (87.50%) - LeetSync

---
+++ 3852-path-existence-queries-in-a-graph-ii/path-existence-queries-in-a-graph-ii.java
@@ -0,0 +1,89 @@
+class Solution {
+    public int[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        
+     int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+
+        int[] pos = new int[n]; // original index -> sorted position
+        int[] val = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            val[i] = arr[i][0];
+            pos[arr[i][1]] = i;
+        }
+
+        // component id
+        int[] comp = new int[n];
+        int cid = 0;
+        comp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (val[i] - val[i - 1] > maxDiff)
+                cid++;
+            comp[i] = cid;
+        }
+
+        // reach[i] = farthest index reachable in one edge
+        int[] reach = new int[n];
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            while (r + 1 < n && val[r + 1] - val[i] <= maxDiff) {
+                r++;
+            }
+            reach[i] = r;
+            if (r == i)
+                r++;
+        }
+
+        int LOG = 18; // 2^17 > 1e5
+        int[][] up = new int[LOG][n];
+        up[0] = reach;
+
+        for (int k = 1; k < LOG; k++) {
+            for (int i = 0; i < n; i++) {
+                up[k][i] = up[k - 1][up[k - 1][i]];
+            }
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int qi = 0; qi < queries.length; qi++) {
+            int u = pos[queries[qi][0]];
+            int v = pos[queries[qi][1]];
+
+            if (u > v) {
+                int t = u;
+                u = v;
+                v = t;
+            }
+
+            if (u == v) {
+                ans[qi] = 0;
+                continue;
+            }
+
+            if (comp[u] != comp[v]) {
+                ans[qi] = -1;
+                continue;
+            }
+
+            int cur = u;
+            int steps = 0;
+
+            for (int k = LOG - 1; k >= 0; k--) {
+                if (up[k][cur] < v) {
+                    cur = up[k][cur];
+                    steps += 1 << k;
+                }
+            }
+
+            ans[qi] = steps + 1;
+        }
+
+        return ans;
+    }
+}
