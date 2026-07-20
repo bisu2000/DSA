a252dd522c20348d3d6239e618898a545eee5e26
Biswajit Manna <162682435+bisu2000@users.noreply.github.com>
2026-07-20 19:13:16 +0530

    Time: 4 ms (97.01%) | Memory: 47.1 MB (79.54%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        
+     int m = grid.length, n = grid[0].length;
+        int size = m*n;
+
+        // Flatten grid into a linear array in row-major order
+        int temp[] = new int[size];
+        int ind = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                temp[ind++] = grid[i][j];
+            }
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        ind = 0;
+        for(int i=0; i<m; i++) {
+            List<Integer> list = new ArrayList<>();
+            for(int j=0; j<n; j++) {
+                // Source index is k behind; (+size)%size keeps it non-negative for the wrap
+                int idx = ((ind-k)%size + size ) %size;
+                int val = temp[idx];
+                list.add(val);
+                ind++;
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+}
